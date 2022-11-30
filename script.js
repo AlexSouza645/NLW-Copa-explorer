4376ce8ef6d9984ee7879ba006de3a0b59f59ebc
AlexSouza645 <59890212+AlexSouza645@users.noreply.github.com>
2022-11-30 13:00:29 -0300

    criando icones em js

---
+++ script.js
@@ -0,0 +1,42 @@
+
+function createGame(player1, hour, player2) {
+
+  return `
+  <li>
+     <img src="./assets/icon-${player1}.svg" alt="Bandeira do ${player1}" />
+     <strong>${hour}</strong>
+     <img src="./assets/icon-${player2}.svg" alt="Bandeira do ${player2}" />
+           
+           
+           
+   </li>
+
+  `
+}
+
+
+function createCard(date, day,games) {
+
+  return `
+    <div class="card">
+          <h2>${date}<span>${day}</span></h2>
+          <ul>
+            
+            ${games}
+          </ul>
+        </div>
+    
+    `
+}
+
+document.querySelector('#app').innerHTML = `
+
+<header>
+        <img src="./assets/logo.svg" alt="" />
+      </header>
+      <main id="cards">
+        ${createCard('24/11', 'quinta', createGame('Brazil','16:00', 'Serbia'))}
+        ${createCard('28/11', 'segunda', createGame('Switizerland','13:00','Brazil'))}
+        ${createCard('02/12', 'sexta', createGame( 'Brazil','16:00','Cameroon'))}  
+      </main>
+`
