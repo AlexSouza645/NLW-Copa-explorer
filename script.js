356a05ac938dfbd34fd5b43508d17b280c648e5c
AlexSouza645 <59890212+AlexSouza645@users.noreply.github.com>
2022-12-26 12:56:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function createGame(player1, hour, player2) {
   <li>
      <img src="/assets/icon-${player1}.svg" alt="Bandeira do ${player1}" />
      <strong>${hour}</strong>
-     <img src="assets/icon-${player2}.svg" alt="Bandeira do ${player2}" />
+     <img src="/assets/icon-${player2}.svg" alt="Bandeira do ${player2}" />
            
            
            
